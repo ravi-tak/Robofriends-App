aaa5c9bc6f8201d1f67657de45c66069e9f8c400
RAVI KUMAR <ravikumartakajm@gmail.com>
2023-04-02 16:30:23 +0530

    add hooks

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,63 +1,31 @@
 import "./Main.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Main() {
-  //   var fetchData = [];
-  //   const promise = fetch("https://jsonplaceholder.typicode.com/users");
-  //   promise
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //       fetchData = data.map((element) => element);
-  //       console.log(fetchData);
-  //       return fetchData;
-  //     });
+  const [resData, setResData] = useState([]);
+  const [data, setData] = useState([]);
 
-  // getting data from other file(know more about it)
-  const fetchData = require("../Data.json");
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setResData(data);
+        setData(data);
+      });
+  }, []);
 
-  // Creating divs that will show up on rendering
-  let divs = fetchData.map((data) => {
-    return (
-      <div key={data.id}>
-        <img src={`https://robohash.org/${data.id}`} />
-        <span className="bold">{data.name}</span>
-        <p>{data.email}</p>
-        <p>{data.username}</p>
-      </div>
-    );
-  });
-
-  // To render the change using state concept
-  var [divArray, setDivArray] = useState(divs);
-
-  const names = fetchData.map((data) => data.name.toLowerCase());
-
-  // event handler
   const handler = (event) => {
-    var filterName = names.filter((name) =>
-      name.includes(event.target.value.toLowerCase())
+    var filterData = resData.filter((item) =>
+      item.name.toLowerCase().includes(event.target.value.toLowerCase())
     );
-
     var newData = [];
-    for (let name of filterName) {
-      for (let getData of fetchData) {
-        if (getData.name.toLowerCase() === name) newData.push(getData);
+    for (let data of filterData) {
+      for (let getData of resData) {
+        if (getData.name.toLowerCase() === data.name.toLowerCase())
+          newData.push(getData);
       }
     }
-
-    divArray = newData.map((newData) => {
-      return (
-        <div key={newData.id}>
-          <img src={`https://robohash.org/${newData.id}`} />
-          <span className="bold">{newData.name}</span>
-          <p>{newData.email}</p>
-          <p>{newData.username}</p>
-        </div>
-      );
-    });
-
-    return setDivArray(divArray);
+    return setData(newData);
   };
 
   return (
@@ -72,44 +40,26 @@ function Main() {
         ></input>
       </div>
 
-      <div className="cards-container">{divArray}</div>
+      <div className="cards-container">
+        {data.map((element) => (
+          <div key={element.id}>
+            <img src={`https://robohash.org/${element.id}`} />
+            <span className="bold">{element.name}</span>
+            <p>{element.email}</p>
+            <p>{element.username}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default Main;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // For me
+
+// getting data from other file(know more about it)
+// const fetchData = require("../Data.json");
 
 // async function getData() {
 //  try {

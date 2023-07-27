42b78c4925de93947f5fa63843f7328aa3498ab7
RAVI KUMAR <ravikumartakajm@gmail.com>
2023-07-27 22:40:53 +0530

    removed comments

--- src/App.js
+++ src/App.js
@@ -10,13 +10,8 @@ function App() {
   useEffect(() => {
     const getResponseData = async () => {
       try {
-        // respone is an axios promise obj it directly returns the JSON data in the response object
-        // and it different from fetch promise obj because the fetch one dosn't contain json data directly 
-        // we have use response.json() to get json data
         const response = await axios("https://jsonplaceholder.typicode.com/users");
-        // When using await, it ensures that the code waits for the Promise to resolve
-        // before proceeding to the next lines of code
-        const jsonData = response.data; // Use response.data directly and it waits untill the promise resolved
+        const jsonData = response.data;
         setApiData(jsonData);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -29,7 +24,6 @@ function App() {
     <div className="App">
       <Title />
       {apiData.length > 0 && <Main apiData={apiData} />}
-      {/* This needs to be done length is not > 0 to show background and search bar from starting */}
       {!apiData.length > 0 && <Main apiData={apiData} />}
     </div>
   );
